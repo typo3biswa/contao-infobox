5527b89e5e6e7d7c0178574223123885b78b17c2
typo3biswa <onlinebiswajit@gmail.com>
2024-03-28 02:38:11 +0530

    Delete system/modules/sharebuttons directory

--- system/modules/sharebuttons/assets/scripts.js
+++
@@ -1,7 +0,0 @@
-// share dialogue
-function shd() {};
-shd.open = function(href,width,height)
-{
-	window.open(href, '', 'width='+width+',height='+height+',location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,toolbar=no');
-	return false;
-};
